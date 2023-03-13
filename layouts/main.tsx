0471d1197dd77fc3c11d469581c4b69b128c90eb
Yann Rocq <yann@rocq.net>
2023-03-13 08:29:32 -0400

    Fix typo

--- layouts/main.tsx
+++ layouts/main.tsx
@@ -34,7 +34,7 @@ export default function Main({children}: Props): JSX.Element {
       {children}
       <footer className="footer">
         <div className="content">
-          &copy; Yann Rocq. Sauf mention contraire, tous les textes de ce sites sont publiés sous licence <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.fr">Creative Commons BY-SA 4.0</a>.    
+          &copy; Yann Rocq. Sauf mention contraire, tous les textes de ce site sont publiés sous licence <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.fr">Creative Commons BY-SA 4.0</a>.    
         </div>
       </footer>
 
